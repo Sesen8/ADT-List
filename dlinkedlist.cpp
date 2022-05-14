f4cd884759dc40eb0a0b26df52e01030376b674e
Sesen8 <yonass@spu.edu>
2022-05-13 18:17:17 -0700

    done
    hjhkahdjkshb

--- dlinkedlist.cpp
+++ dlinkedlist.cpp
@@ -4,7 +4,9 @@
 #include "object.h"
 #include "dlinkedlist.h"
 
+#include <iostream>
 #include <string>
+using namespace std;
 using std::string;
 
 DoubleLinkedList::DoubleLinkedList(const DoubleLinkedList &other){
@@ -15,33 +17,154 @@ DoubleLinkedList &DoubleLinkedList::operator=(const DoubleLinkedList &rhs) {
 }
 
 DoubleLinkedList::DoubleLinkedList(){
+    _head = nullptr;
+    _tail = nullptr;
+    _size = 0;
 
 }
 
 DoubleLinkedList::~DoubleLinkedList() {
+    Node* tmp = _head;
+    while(tmp!=nullptr){
+        Node* next = tmp->next;
+        delete tmp;
+        tmp = next;
+    }
+    delete _head;
+
 
 }
 
 bool DoubleLinkedList::Insert(Object *element, size_t position) {
-	return false;
+    if(position >_size){
+        return false;
+    }
+
+    Node* newNode = new Node();
+    newNode->data = element;
+    newNode->next= nullptr;
+    newNode->previous=nullptr;
+
+    if(_size==0){
+        _head=newNode;
+        _tail=newNode;
+    }
+    else if(position==0){
+        newNode->next=_head;
+        _head->previous=newNode;
+        _head=newNode;
+
+    }
+
+    else {
+        Node *tmp = _head;
+        for (int i = 0; i < position-1; i++) {
+            tmp = tmp->next;
+        }
+
+        newNode->next = tmp->next;
+        newNode->previous = tmp;
+        tmp->next = newNode;
+        if(newNode->next !=nullptr){
+            newNode->next->previous=newNode;
+        }
+
+    }
+
+    _size++;
+	return true;
 }
 
 int DoubleLinkedList::IndexOf(const Object *element) const {
+
+    int i = 0;
+    for (Node* tmp = _head; tmp != nullptr; tmp = tmp->next){
+        if (tmp->data->Equals(*element)){
+            return i;
+        }
+        i++;
+    }
+
 	return -1;
 }
 
 Object *DoubleLinkedList::Remove(size_t position) {
-	return nullptr;
+    if (position >= _size){
+        return nullptr;
+    }
+
+    Object* ptr = nullptr;
+    Node* tmp = new Node();
+    tmp= nullptr;
+
+    if (position == 0){
+        ptr = _head->data;
+        _head->data = nullptr;
+        tmp = _head->next;
+        _head->next = nullptr;
+        delete _head;
+        _head = tmp;
+    }
+    else if (position>0){
+        tmp = _head;
+
+        for (size_t i = 0; i < position -1; i++){
+            tmp = tmp->next;
+        }
+
+        Node* deleteNode = new Node();
+        deleteNode = tmp->next;
+        ptr = deleteNode->data;
+        tmp->next = deleteNode->next;
+        deleteNode->next = nullptr;
+        deleteNode->data = nullptr;
+        delete deleteNode;
+    }
+    _size--;
+    return ptr;
+
 }
 
 Object *DoubleLinkedList::Get(size_t position) const {
-	return nullptr;
+    if (position >= _size){
+        return nullptr;
+    }
+    Node* tmp = _head;
+    for (size_t i = 0; i < position; i++) {
+        tmp = tmp->next;
+    }
+    return tmp->data;
+
 }
 
 string DoubleLinkedList::ToString() const {
-	return "";
+    string returnString;
+    returnString = "{";
+
+    Node* tmp = new Node;
+
+    for(tmp=_head;tmp!=nullptr;tmp=tmp->next){
+        returnString = tmp->data->ToString() + " ";
+    }
+
+    returnString = tmp->data->ToString() + "}";
+
+	return returnString;
 }
 
 void DoubleLinkedList::Clear() {
+    Node* tmp = _head;
+
+    while (tmp != nullptr){
+        if (tmp->data)
+            delete tmp->data;
+
+        Node* toDelete = tmp;
+        tmp = tmp->next;
+        delete toDelete;
+    }
+    _head = nullptr;
+    _tail=nullptr;
+    _size = 0;
 
 }
--- vsarray.cpp
+++ vsarray.cpp
@@ -10,37 +10,161 @@
 using std::string;
 
 VSArray::VSArray(size_t capacity, double increasePercentage){
-}
-VSArray::VSArray(const VSArray& list){
-}
-VSArray::~VSArray(){
-}
-const VSArray& VSArray::operator=(const VSArray& rhs){
-	return *this;
+    _capacity=(capacity);
+    _data = new Object*[_capacity];
+    for (size_t i = 0; i < _capacity; i++) {
+        _data[i] = nullptr;
+    }
+
 }
 
-bool VSArray::Insert(Object* element, size_t position){
-	return false;
+
+VSArray::VSArray(const VSArray& list){
+    _size = list._size;
+    _data = new Object*[_capacity];
+    for (size_t i = 0; i < _size; i++) { // Issa correction to the _capacity
+        _data[i] = list.Get(i)->Clone();
+    }
+    for (size_t i = _size; i < _capacity; i++){
+        _data[i] = nullptr;
+    }
+
+
 }
+
+
+
+VSArray::~VSArray(){
+    for(int i=0; i<_capacity; i++){
+        delete _data[i];
+    }
+    delete[] _data;
+}
+
+
+
+const VSArray& VSArray::operator=(const VSArray& rhs){
+    if (&rhs == this){
+        return *this;
+    }
+    if (_capacity != rhs._capacity){
+        Clear();
+        delete[] _data;
+        _capacity = rhs._capacity;
+        _data = new Object*[_capacity];
+        for (size_t i = 0; i < _capacity; i++) {
+            _data[i] = nullptr;
+        }
+    }
+    for (size_t i = 0; i < rhs.Size(); i++) {
+        _data[i] = rhs.Get(i)->Clone();
+    }
+    return *this;
+}
+
+
+
+
+
+bool VSArray::Insert(Object* element, size_t position) {
+    if (position > _size) {
+        return false;
+    }
+    if (_capacity == _size){
+        Resize();
+    }
+    if(position<_size){
+        for(size_t i =_size; i>position; i-- ){
+            _data[i] = _data[i-1];
+        }
+
+    }
+    _data[position] = element;
+    _size++;
+	return true;
+}
+
+
+
 int VSArray::IndexOf(const Object* element)const{
+    for (size_t i = 0; i < _size; i++) {
+        if (_data[i]->Equals(*element)){
+            return i;
+        }
+    }
+
 	return -1;
 }
+
+
 Object* VSArray::Remove(size_t position){
-	return nullptr;
-}
-Object* VSArray::Get(size_t position)const{
-	return nullptr;
-}
-string VSArray::ToString()const{
-	return "";
-}
-void VSArray::Clear(){
+    if (position >= _size){
+        return nullptr;
+    }
+    Object* retVal = _data[position];
+    for (size_t i = position; i < _size - 1; i++) {
+        _data[i] = _data[i + 1];
+    }
+    _data[_size - 1] = nullptr;
+    _size--;
+    return retVal;
 
 }
+
+
+Object* VSArray::Get(size_t position)const {
+    if (position >= _size) {
+        return nullptr;
+    }
+    else {
+        return _data[position];
+    }
+
+}
+
+
+
+string VSArray::ToString()const{
+    string retVal;
+    retVal = "{";
+
+    for (size_t i = 0; i < _size - 1; i++) {
+        retVal = _data[i]->ToString() + ", ";
+    }
+    retVal =_data[_size - 1]->ToString() + "}";
+    return retVal;
+
+}
+
+
+
+void VSArray::Clear(){
+    for (size_t i = 0; i < _size; i++) {
+        delete _data[i];
+        _data[i] = nullptr;
+    }
+    _size = 0;
+
+}
+
+
 
 void VSArray::Resize() {
+    size_t newCapacity = static_cast<size_t>(_capacity * 1.5);
+	Object** newData = new Object*[newCapacity];
+	for (size_t i = 0; i < _size; i++) {
+		newData[i] = _data[i];
+	}
+	for (size_t i = _size; i < newCapacity; i++) {
+		newData[i] = nullptr;
+	}
+	delete[] _data;
+	_data = newData;
+	_capacity = newCapacity;
 
 }
+
+
 
 size_t VSArray::GetCapacity() const {
 	return _capacity;
